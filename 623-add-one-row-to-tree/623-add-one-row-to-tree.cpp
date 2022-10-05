fdc1700ba9dc0aa83b1a0aebd80f8dd62267ed99
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2022-10-05 20:27:11 +0530

    Time: 35 ms (49.40%), Space: 25.3 MB (5.68%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -1,66 +1,50 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+class Solution
+{
 public:
-    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
-         
-        if(root == NULL)
+    TreeNode *addOneRow(TreeNode *root, int val, int depth)
+    {
+
+        if (root == NULL)
             return root;
-        if(depth==1)
+        if (depth == 1)
         {
-            TreeNode *node=new TreeNode(val);
-            node->left=root;
+            TreeNode *node = new TreeNode(val);
+            node->left = root;
             return node;
         }
-        queue<TreeNode*> q;
-            q.push(root);
-        int level=0;
-        while(!q.empty()){ 
+        queue<TreeNode *> q;
+        q.push(root);
+        int level = 0;
+        while (!q.empty())
+        {
             int n = q.size();
-           vector<int>v1;
+            vector<int> v1;
             level++;
-            if(depth-1==level)
-            { for(int i=0;i<n;i++)
+            if (depth - 1 == level)
+            {
+                for (int i = 0; i < n; i++)
                 {
-                    cout<<i<<" ";
-                    TreeNode *cu=q.front();
+                    TreeNode *cu = q.front();
                     q.pop();
-                     TreeNode *no1=new TreeNode(val,cu->left,NULL);
-                        cu->left=no1;
-                     TreeNode *no2=new TreeNode(val,NULL,cu->right);
-                        cu->right=no2;
-//                   if(!cu->left&&!cu->right)
-//                     {
-//                         TreeNode *n1=new TreeNode(val,NULL,NULL);
-//                         TreeNode *n2=new TreeNode(val,NULL,NULL);
-                          
-//                         cu->left=n1;
-//                         cu->right=n2;
-//                     }
+                    TreeNode *no1 = new TreeNode(val, cu->left, NULL);
+                    cu->left = no1;
+                    TreeNode *no2 = new TreeNode(val, NULL, cu->right);
+                    cu->right = no2;
                 }
                 return root;
             }
-              for(int i =0; i<n; i++){
-                TreeNode * current = q.front();
-                        v1.push_back(current->val);
+            for (int i = 0; i < n; i++)
+            {
+                TreeNode *current = q.front();
+                v1.push_back(current->val);
                 q.pop();
-                if(current->left != NULL)
+                if (current->left != NULL)
                     q.push(current->left);
-                if(current->right != NULL)
+                if (current->right != NULL)
                     q.push(current->right);
             }
-            
         }
-        
-       return root;     
+
+        return root;
     }
 };
