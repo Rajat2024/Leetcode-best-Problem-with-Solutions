c7c4a3f0aac7ba399b62d5647b3568d23a3566be
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2022-11-04 18:40:43 +0530

    Added solution - LeetHub

--- Base Equivalence - GFG/base-equivalence.cpp
+++ Base Equivalence - GFG/base-equivalence.cpp
@@ -6,37 +6,23 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 class Solution {
-    char reVal(int num)
-{
-    if (num >= 0 && num <= 9)
-        return (char)(num + '0');
-    else
-        return (char)(num - 10 + 'A');
-}
-    string fromDeci(string& res, int base, int inputNum)
-     {
-    int index = 0; 
-    while (inputNum > 0) {
-        res.push_back(reVal(inputNum % base));
-        index++;
-        inputNum /= base;
-    }
-    reverse(res.begin(), res.end());
-    return res;
-   }
   public:
     string baseEquiv(int n, int m){
-        string res="";
-        for(int i=2;i<32;i++)
+        // code here
+         for(int i = 2; i <= 32; i++)
         {
-            res="";
-            string str=fromDeci(res,i,n);
-            if(str.size()==m)
+            int count = 0;
+            int cur = n;
+            while(cur != 0)
+            {
+                count++;
+                cur /= i;
+                if(count > m)break;
+            }
+            if(count == m)
             return "Yes";
         }
         return "No";
-        
-        
     }
 };
 
