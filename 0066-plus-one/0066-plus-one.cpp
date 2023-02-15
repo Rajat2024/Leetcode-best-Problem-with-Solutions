ea906699bce9348f5289ec168f4529308f786c4c
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-02-15 09:21:00 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (18.87%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& num) {
+        int k=1;
+         vector<int>ans(num.size(),0);
+        for(int i=num.size()-1;i>=0;i--)
+        {
+            ans[i]=num[i]+k;
+            k=ans[i]/10; 
+            ans[i]%=10;
+        }
+        reverse(ans.begin(),ans.end());
+        while(k>0)
+        {
+            ans.push_back(k%10);
+            k/=10;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
