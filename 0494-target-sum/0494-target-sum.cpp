d2a423322d71c8d6f339c6d0d4dcc555410df4eb
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-03-03 20:39:53 +0530

    Time: 1164 ms (22.90%), Space: 439.3 MB (5.05%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
     int rec(vector<int>& nums, int target,int i)
     {
-        
+      
         if(nums.size()==i)
         {
-            if(target==0)
-                return 1;
-            else
-                return 0;
+            if(target==0) return 1;
+          return 0;
         }
-        
-        int take=0;
-            take=rec(nums,target-nums[i],i+1);
-        
-        int not_take=rec(nums,target+nums[i],i+1);
-        return take+not_take;
+        if(dp[1000+target][i]!=-1)  return dp[1000+target][i];
+          return dp[1000+target][i]=rec(nums,target-nums[i],i+1) + rec(nums,target+nums[i],i+1);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        dp.resize(30001,vector<int>(nums.size(),-1));
         return rec(nums,target,0);
-    }
+    }
 };
