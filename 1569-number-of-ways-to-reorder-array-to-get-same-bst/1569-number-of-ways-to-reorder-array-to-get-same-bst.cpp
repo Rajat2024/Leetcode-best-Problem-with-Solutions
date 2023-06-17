46971bf82e81f167ccc6c297e665acd13dcf9e2a
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-06-17 07:13:06 +0530

    Time: 1146 ms (5.29%), Space: 431.1 MB (5.30%) - LeetHub

--- 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.cpp
+++ 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.cpp
@@ -35,7 +35,7 @@ class Solution
                 right.push_back(nums[i]);
         }
         long long leftCount = total_required_BST(left);
-        long long rightCount = total_required_BST(right);
+        long long rightCount = total_required_BST(right); 
         long long totalCount = binomialCoefficient(left.size() + right.size(), left.size());
 
         return (((leftCount * rightCount) % MOD) * totalCount % MOD);
@@ -44,6 +44,6 @@ class Solution
 public:
     int numOfWays(vector<int> &nums)
     {
-        return (total_required_BST(nums) - 1 + MOD) % MOD;
+        return (total_required_BST(nums)-1);
     }
 };
