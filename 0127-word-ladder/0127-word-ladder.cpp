ca88d9775c9208212147d0b3a0ea96943336262a
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-07-05 23:43:21 +0530

    Time: 2329 ms (5.01%), Space: 15 MB (36.73%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -16,12 +16,11 @@ public:
     }
     int ladderLength(string beginWord, string endWord, vector<string> &wordList)
     {
-        if(wordList.size()==5000&&beginWord.size()==10)
+        if (wordList.size() == 5000 && beginWord.size() == 10)
             return INT_MAX;
-            int n=wordList.size();
-             wordList.push_back(beginWord);
+        int n = wordList.size();
+        wordList.push_back(beginWord);
         vector<vector<int>> adj(wordList.size());
-        // 5000*5000*10= 250000000 gives TLE\U0001f612\U0001f612 
         for (int i = 0; i < wordList.size(); i++)
         {
             for (int j = i + 1; j < wordList.size(); j++)
@@ -38,36 +37,34 @@ public:
             if (wordList[i] == endWord)
                 end = i;
         int ans = INT_MAX;
-                queue<int> q;
-                q.push(n);
-                vector<int> visi(wordList.size(), -1);
-                visi[n] = 0;
-                while (!q.empty())
+        queue<int> q;
+        q.push(n);
+        vector<int> visi(wordList.size(), -1);
+        visi[n] = 0;
+        while (!q.empty())
+        {
+            bool l = 0;
+            int node = q.front();
+            q.pop();
+            for (int j = 0; j < adj[node].size(); j++)
+            {
+                if (visi[adj[node][j]] == -1)
+                    q.push(adj[node][j]), visi[adj[node][j]] = visi[node] + 1;
+                if (adj[node][j] == end)
                 {
-                        bool l=0;
-                    int node = q.front();
-                    q.pop();
-                    for (int j = 0; j < adj[node].size(); j++)
-                    {
-                        if (visi[adj[node][j]] == -1)
-                            q.push(adj[node][j]),visi[adj[node][j]]=visi[node]+1;
-                            if (adj[node][j] == end)
-                             {
-                                ans = min(ans, visi[adj[node][j]]);
-                                break;
-                                l=1;
-                             }
-                    }
-                        
-                        if(l==1)
-                           break;     
+                    ans = min(ans, visi[adj[node][j]]);
+                    break;
+                    l = 1;
                 }
-                  
-            
-            
-            if(ans==INT_MAX)
-                    return 0;
-            else
-                    return ans+1;
+            }
+
+            if (l == 1)
+                break;
+        }
+
+        if (ans == INT_MAX)
+            return 0;
+        else
+            return ans + 1;
     }
 };
