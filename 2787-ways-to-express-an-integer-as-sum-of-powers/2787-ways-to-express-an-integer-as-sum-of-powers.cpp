5cda8b910e933064f8ba016de57a1a5ba9a0cfda
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-07-23 07:05:58 +0530

    Time: 402 ms (57.14%), Space: 6.8 MB (71.43%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,52 +1,30 @@
-const long long mod = 1000000007;
-long long power_with_modulo(long long x, long long y)
-{
-    // a^b when a and b are long long with modules with TC -> O(log(n))
-    long long res = 1;
-    x = x % mod;
-    if (x == 0)
-    {
-        return 0;
-    }
-    while (y > 0)
-    {
-        if (y & 1)
-        {
-            res = (res * x) % mod;
-        }
-        y = y >> 1;
-        x = (x * x) % mod;
-    }
-    return res % mod;
-}
-long long numSubseq(vector<long long>& arr, int k) {
-        int n = arr.size();
-    vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, 0));
-
-    // Initialization
-    dp[0][0] = 1;
-
-    // Filling the dp array
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 0; j <= k; ++j) {
-            dp[i][j] = dp[i - 1][j];
-            if (j >= arr[i - 1]) 
-            {
-                dp[i][j] += dp[i - 1][j - arr[i - 1]]%mod ;
-            }
-        }
-    }
-
-    return dp[n][k]%mod;
-    }
 class Solution {
 public:
-    int numberOfWays(int n, int x) {
-        vector<long long>dp(n);
-          for(int i=1;i<=n;i++)
-          {
-              dp[i-1]=power_with_modulo(i,x)%mod;
-          }
-        return numSubseq(dp,n)%mod;
+     int mod = 1e9+7;
+     int dp[301][301];
+    
+     int f(int n, int num, int x ) 
+     {
+            if(n < 0 ) return 0;  // if number n becomes -ve
+            
+            if(n == 0) return 1; // we ave found number
+         
+            if(pow(num , x) > n) return 0; // we will never get the number n
+         
+            if(dp[n][num] != -1) return dp[n][num]; // memo
+          
+            int temp = pow(num , x) ;
+            
+            int pick = f(n- temp , num + 1 , x)%mod; 
+            int skip = f(n , num + 1 , x)%mod;
+            
+            return dp[n][num] =  (skip  + pick ) % mod ;
     }
+    
+    
+    int numberOfWays(int n, int x) {
+        memset(dp, -1, sizeof(dp));
+        return f(n, 1, x);
+    }
+
 };
