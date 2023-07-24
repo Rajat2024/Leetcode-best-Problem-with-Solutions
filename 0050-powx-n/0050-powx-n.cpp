99d47409dac156a5cbad09bde91de5fd2f96d130
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-07-24 23:07:19 +0530

    Time: 2 ms (45.63%), Space: 6 MB (62.65%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -6,10 +6,10 @@ public:
         double ans = 1;
         if (n < 0)
         {
-            n = abs(n);
             x = 1 / x;
         }
-
+        long num=labs(n);
+        
         if (x == 1)
             return x;
         if (x == -1)
@@ -19,28 +19,26 @@ public:
             else
                 return abs(x);
         }
-        while (n)
+        while (num)
         {
-            if (x==0)
-            {
-                // cout<<setprecision(7);
-                // cout<<x;
-                return 0.00000;
-            }
-            if (n % 2)
+            // if (x==0.00000)
+            // {
+            //     return 0.00000;
+            // }
+            if (num % 2)
             {
                 ans *= x;
 
-                if (n == 1)
+                if (num == 1)
                 {
                     break;
                 }
-                n--;
+                num--;
             }
             else
             {
                 x = x * x;
-                n = n / 2;
+                num = num / 2;
             }
         }
 
