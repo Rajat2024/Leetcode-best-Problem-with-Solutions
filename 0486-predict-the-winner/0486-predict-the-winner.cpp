5388d20ebbb408785472e8878bdd06e6ebaab2b2
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-07-29 16:54:26 +0530

    Time: 3 ms (70.49%), Space: 7.4 MB (54.93%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool rec(vector<int>& nums, int i, int j, int s1, int s2, bool cc) {
+        if (i > j)
+            return s1 >= s2;
+
+        if (cc)
+            return (rec(nums, i + 1, j, s1 + nums[i], s2, false) || rec(nums, i, j - 1, s1 + nums[j], s2, false));
+        else
+            return (rec(nums, i + 1, j, s1, s2 + nums[i], true) && rec(nums, i, j - 1, s1, s2 + nums[j], true));
+    }
+
+    bool PredictTheWinner(vector<int>& nums) {
+        int n = nums.size() - 1;
+        return rec(nums, 0, n, 0, 0, true);
+    }
+};
