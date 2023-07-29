7a4927c9c726a2e359a0229f5825edd2080d7cfa
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-07-29 16:59:13 +0530

    Time: 6 ms (53.02%), Space: 7.3 MB (75.50%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -8,6 +8,8 @@ public:
             return (rec(nums, i + 1, j, s1 + nums[i], s2, false) || rec(nums, i, j - 1, s1 + nums[j], s2, false));
         else
             return (rec(nums, i + 1, j, s1, s2 + nums[i], true) && rec(nums, i, j - 1, s1, s2 + nums[j], true));
+        // here we use && becuse if T||rec() then next call will not be happen so we want if T||rec() then next rec must be run 
+        // 
     }
 
     bool PredictTheWinner(vector<int>& nums) {
